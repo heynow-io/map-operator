57d20ac978802ec0e50db2d2f3e055ef49fa63d1
konrad.chmiel <konrad.chmiel@ocado.com>
2016-10-19 14:56:59 +0200

    update properties to use eureka and properties service

--- src/main/java/io/heynow/mapoperator/Application.java
+++ src/main/java/io/heynow/mapoperator/Application.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 
 @Slf4j
 @EnableBinding(Sink.class)
 @EnableCaching
+@EnableEurekaClient
 @EnableStreamManagerClient
 @SpringBootApplication
 public class Application {
